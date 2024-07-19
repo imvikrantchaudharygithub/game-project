dd7ea9ce03ba737b103833e7a944ed0dae8bd384
Vikrant Chaudhary <vikrantchaudhary@Vikrants-MacBook-Pro.local>
2024-07-19 17:04:17 +0530

    socket added in color game04

--- src/pages/services/tokenservice.tsx
+++ src/pages/services/tokenservice.tsx
@@ -6,20 +6,21 @@ if (typeof window !== 'undefined' && window.localStorage) {
   localStorageAvailable = true;
 }
 
-export const setToken = (token:any) => {
+const setToken = (token:any) => {
   if (localStorageAvailable) {
     localStorage.setItem('accessToken', token);
   }
 };
 
-export const getToken = () => {
+const getToken = () => {
   if (localStorageAvailable) {
     return localStorage.getItem('accessToken');
   }
   return null;
 };
-export const clearTokenlocal = () => {
+const clearTokenlocal = () => {
   if (localStorageAvailable) {
     localStorage.removeItem('accessToken');
   }
 };
+export default {setToken,getToken,clearTokenlocal};
