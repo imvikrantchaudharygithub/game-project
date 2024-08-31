a75f6b20300ae3e1dbe579c88bc39cf139df90a8
Himanshu94Ghosh <himanshughosh882603@gmail.com>
2024-09-01 00:44:48 +0530

    rock page mobile ui done

--- src/pages/rock.tsx
+++ src/pages/rock.tsx
@@ -15,7 +15,7 @@ export default function Rock() {
             box.addEventListener("click", () => {
             const boxRect = box.getBoundingClientRect();
         
-            monkey.style.top = `${boxRect.top + boxRect.height / 2 - 40}px`;
+            monkey.style.top = `${boxRect.top + boxRect.height / 2 - 35}px`;
             monkey.style.left = `${boxRect.left + boxRect.width / 2 - 40}px`;
 
             });
@@ -31,11 +31,13 @@ export default function Rock() {
             <section className="rock">
                 <div className="container">
                     <div className="rock-main d-flex align">
-                        <div className="rock-btn first-btn" id="box22" ref={(el) => (boxesRef.current[21] = el)}>
-                            <Image  width={80} height={80} className="" src={'/assets/images/monkey-icon.png'}  alt=""></Image>
-                        </div>
-                        <div className="rock-btn second-btn" id="monkey" ref={monkeyRef}>
-                            <Image  width={80} height={80} className="" src={'/assets/images/monkey-icon.png'}  alt=""></Image>
+                        <div className="w-full">
+                            <div className="rock-btn first-btn" id="box22" ref={(el) => (boxesRef.current[21] = el)}>
+                                <Image  width={80} height={80} className="" src={'/assets/images/monkey-icon.png'}  alt=""></Image>
+                            </div>
+                            <div className="rock-btn second-btn" id="monkey" ref={monkeyRef}>
+                                <Image  width={80} height={80} className="" src={'/assets/images/monkey-icon.png'}  alt=""></Image>
+                            </div>
                         </div>
                         {/* <div className="rock-btn">
                             <div id="monkey" ref={monkeyRef}>
@@ -48,35 +50,29 @@ export default function Rock() {
                                 <div className="rock-box" id="box2" ref={(el) => (boxesRef.current[1] = el)}></div>
                                 <div className="rock-box" id="box3" ref={(el) => (boxesRef.current[2] = el)}></div>
                                 <div className="rock-box" id="box4" ref={(el) => (boxesRef.current[3] = el)}></div>
+                                <div className="rock-box" id="box5" ref={(el) => (boxesRef.current[4] = el)}></div>
                             </div>
                             <div className="rock-card">
-                                <div className="rock-box" id="box5" ref={(el) => (boxesRef.current[4] = el)}></div>
                                 <div className="rock-box" id="box6" ref={(el) => (boxesRef.current[5] = el)}></div>
                                 <div className="rock-box" id="box7" ref={(el) => (boxesRef.current[6] = el)}></div>
                                 <div className="rock-box" id="box8" ref={(el) => (boxesRef.current[7] = el)}></div>
-                            </div>
-                            <div className="rock-card">
                                 <div className="rock-box" id="box9" ref={(el) => (boxesRef.current[8] = el)}></div>
                                 <div className="rock-box" id="box10" ref={(el) => (boxesRef.current[9] = el)}></div>
-                                <div className="rock-box" id="box11" ref={(el) => (boxesRef.current[10] = el)}></div>
-                                <div className="rock-box" id="box12" ref={(el) => (boxesRef.current[11] = el)}></div>
                             </div>
                             <div className="rock-card">
+                                <div className="rock-box" id="box11" ref={(el) => (boxesRef.current[10] = el)}></div>
+                                <div className="rock-box" id="box12" ref={(el) => (boxesRef.current[11] = el)}></div>
                                 <div className="rock-box" id="box13" ref={(el) => (boxesRef.current[12] = el)}></div>
                                 <div className="rock-box" id="box14" ref={(el) => (boxesRef.current[13] = el)}></div>
                                 <div className="rock-box" id="box15" ref={(el) => (boxesRef.current[14] = el)}></div>
-                                <div className="rock-box" id="box16" ref={(el) => (boxesRef.current[15] = el)}></div>
-                            </div>
-                            <div className="rock-card">
-                                <div className="rock-box" id="box17" ref={(el) => (boxesRef.current[16] = el)}></div>
-                                <div className="rock-box" id="box18" ref={(el) => (boxesRef.current[17] = el)}></div>
-                                <div className="rock-box" id="box19" ref={(el) => (boxesRef.current[18] = el)}></div>
-                                <div className="rock-box" id="box20" ref={(el) => (boxesRef.current[19] = el)}></div>
                             </div>
                         </div>
-                        <div className="rock-btn" id="box21" ref={(el) => (boxesRef.current[20] = el)}>
-                            <Image width={200} height={200} className="w-full" src={'/assets/images/banana-icon.png'}  alt=""></Image>
+                        <div className="w-full">
+                            <div className="rock-btn" id="box21" ref={(el) => (boxesRef.current[20] = el)}>
+                                <Image width={200} height={200} className="w-full" src={'/assets/images/banana-icon.png'}  alt=""></Image>
+                            </div>
                         </div>
+                        
                     </div>
                 </div>
             </section>
